d2bbe240e564f756d09e77fdc9e58a9cc31c0d23
unic0rn9k <unic0rn9k@protonmail.com>
2022-09-26 12:04:34 +0200

    `()` can be sent between `TaskNode`s

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,14 +1,14 @@
 use crate::MorphicIO;
 
 macro_rules! copy {
-    ($($t: ty)*) => {
+    ($($t: tt)*) => {
         $(unsafe impl MorphicIO for $t {
             const IS_COPY: bool = true;
         })*
     };
 }
 
-copy! (u8 u32 u64 i8 i32 i64 f32 f64 usize isize bool char i128 u128);
+copy! (u8 u32 u64 i8 i32 i64 f32 f64 usize isize bool char i128 u128 ());
 
 unsafe impl MorphicIO for String {}
 unsafe impl<'a> MorphicIO for &'a str {}
